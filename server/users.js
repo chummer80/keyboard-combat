605072079817b55b2117ceda6c77146d679197d1
chummer80 <mike.c.chu@gmail.com>
2015-07-15 21:55:28 -0700

    fix bug that didn't destroy the game when 2 players both disconnect

--- server/users.js
+++ server/users.js
@@ -18,7 +18,7 @@ Accounts.onCreateUser(function(options, user) {
 UserStatus.events.on("connectionLogout", function(fields) {
 	// if a game is in "playing" state and a player leaves, the other player auto-wins
 	var game = Games.findOne({"players.id": fields.userId, status: "playing"});
-	if (game) {
+	if (game && game.players.length > 1) {
 		var gameId = game._id;
 		var selfIndex = (game.players[0].id === fields.userId) ? 0 : 1;
 		var opponentIndex = (game.players[0].id === fields.userId) ? 1 : 0;
